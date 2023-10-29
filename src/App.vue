984b1befa53dd610e943740a8b576883482d71fd
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-10-28 23:50:33 -0300

    Modal de ajuda

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="h-full">
     <header-component></header-component>
     <form-component></form-component>
+    <help-modal></help-modal>
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
 import FormComponent from './components/FormComponent.vue'
+import HelpModal from './components/HelpModal.vue';
 
 export default {
   name: 'App',
   components: {
     FormComponent,
-    HeaderComponent
+    HeaderComponent,
+    HelpModal
   }
 }
 </script>
---
+++ src/components/HelpModal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="modal fade" id="help-modal" tabindex="-1" aria-labelledby="help-modal-title" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content border-0 shadow-sm">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="help-modal-title">Ajuda</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        O TCC Review não exige autenticação. No entanto, é essencial que seu texto no Google Docs
+                        esteja público. Por favor, certifique-se de que na opção de compartilhamento esteja selecionada
+                        a opção de <strong>qualquer pessoa com link</strong>.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelpModal'
+    }
+</script>
